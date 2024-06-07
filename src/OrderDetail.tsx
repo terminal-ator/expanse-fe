15d536f3dd10fa1a4cabd9518801c13b427d05de
Tushar Garg <itushargarg@gmail.com>
2024-06-07 10:29:44 +0000

    fixed build error

--- src/OrderDetail.tsx
+++ src/OrderDetail.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { useQuery } from "react-query";
+import { useQueries, useQuery } from "react-query";
 import { useParams } from "wouter";
 import pb from "./pb";
 import { CartItem as ICartItem } from "./types";
@@ -8,18 +8,22 @@ import CartItem from "./components/CartItem";
 const OrderDetail = () => {
   const params = useParams();
 
-  const { data: order, isLoading: orderLoading } = useQuery([params.id], () =>
-    pb.collection("order").getOne(params.id!)
-  );
+  const results = useQueries([
+    {
+      queryKey: ["orders", params.id],
+      queryFn: () => pb.collection("orders").getOne(params.id!),
+    },
+    {
+      queryKey: ["lines", params.id],
+      queryFn: () =>
+        pb.collection("orderlines").getFullList<ICartItem>({
+          filter: `of_order = '${params.id}'`,
+          expand: "of_product",
+        }),
+    },
+  ]);
 
-  const { data: lines, isLoading } = useQuery([params.id], () =>
-    pb.collection("orderlines").getFullList<ICartItem>({
-      filter: `of_order = '${params.id}'`,
-      expand: "of_product",
-    })
-  );
-
-  if (isLoading || orderLoading) {
+  if (results[0].isLoading || results[1].isLoading) {
     return (
       <div className="p-2">
         <div className="loading"></div>
@@ -30,9 +34,33 @@ const OrderDetail = () => {
   return (
     <div className="p-2">
       <h1>Viewing order: {params.id}</h1>
-      <div className="badge ">{order?.status}</div>
-      {lines?.map((x) => (
+      <div className="flex flex-row justify-between">
+        <div className="badge badge-primary">
+          {results[0]?.data?.order_status}
+        </div>
+        <div>
+          {results[0]?.data?.order_status === "pending" ? (
+            <div>
+              <button
+                onClick={() => {
+                  confirm("Do you really want to cancel this order?");
+                }}
+                className="btn btn-error text-white"
+              >
+                Cancel
+              </button>
+            </div>
+          ) : (
+            <div>
+              Order has been fulfilled, call <a>7982255302</a> for queries
+            </div>
+          )}
+        </div>
+      </div>
+      <div className="font-bold">Items</div>
+      {results[1].data?.map((x) => (
         <CartItem
+          onRemove={() => {}}
           immutableMode={true}
           key={x.id}
           p={x.expand.of_product}
